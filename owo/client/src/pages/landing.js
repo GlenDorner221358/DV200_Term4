2fbc5caf6d0bd7b201cd049d6b060f5a2fdcb056
CWKrahtz <221198@virtualwindow.co.za>
2023-09-25 16:50:00 +0200

    Login & Signup
    Login/signup and logout works

---
+++ owo/client/src/pages/landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import BasicNav from '../componants/navbar'
+
+function Landing() {
+    return (
+        <div>
+            <BasicNav />
+            <h3 style={{textAlign:'center', margin:'2%'}}>Welcome to Open Window Overflow where you can ask your dev questions and people can give solutions.</h3>
+        </div>
+    )
+}
+
+export default Landing;
---
+++ owo/server/index.js
@@ -0,0 +1,36 @@
+require('dotenv/config')
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const userRoutes = require('./routes/users')
+const authRoutes = require('./routes/auth')
+
+//Used with React()
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use(ProductRoute)
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'Open-Window-Overflow' //Collection Name
+}).then(() => {
+    console.log("Connected to DB")
+}).catch((err) => {
+    console.log("No connection. Error: " + err)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log("Server Started On Port: ", PORT)
+})
