cc342917a11769e273472df3f4565f8e57efcca9
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-10-08 19:39:14 +0200

    Question Updated

--- owo/client/src/pages/Questions.jsx
+++ owo/client/src/pages/Questions.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BasicNav from '../componants/navbar'
 import styles from '../pages/Questions.module.css'
+import Button from 'react-bootstrap/Button';
 function Questions() {
   return (
     <>
@@ -11,11 +12,11 @@ function Questions() {
         <div className={styles.questions_section}>
             <div className={styles.top_container}>
                 <h1 className={styles.question_heading}>Questions</h1>
+                <Button className={styles.ask_btn} variant="primary">Ask Question</Button>{''}
             </div>
             <div className={styles.questions_box}>
-                
             </div>
-
+            <div className={styles.questions_group_container}></div>
         </div>
         <div className={styles.right_panel}></div>
 
